5f15b9856ae4bf0282a517bf22b397703f8039e7
snfsjn22 <jh1710644231@163.com>
2022-09-01 13:45:46 +0800

    modified: 将树形内容单独抽提组件

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+    <el-col>
+      <!-- 左侧内容 -->
+      <span>{{ treeNode.name }}</span>
+      <!-- /左侧内容 -->
+    </el-col>
+    <el-col :span="4">
+      <!-- 右侧内容 -->
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 放置下拉菜单 -->
+          <el-dropdown>
+            <!-- 内容 -->
+            <span> 操作<i class="el-icon-arrow-down" /> </span>
+            <!-- 具名插槽 -->
+            <el-dropdown-menu slot="dropdown">
+              <!-- 下拉选项 -->
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+      <!-- /右侧内容 -->
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    treeNode: {
+      type: Object,
+      required: true // 使用组件的的时候必须传入treeNode属性，不传会报错
+    },
+    isRoot: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,59 +3,13 @@
     <div class="app-container">
       <!-- 组织架构头部内容 -->
       <el-card class="tree-card">
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-          <el-col>
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <!-- 两个内容 -->
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 下拉菜单 element -->
-                <el-dropdown>
-                  <span> 操作<i class="el-icon-arrow-down" /> </span>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <!-- 结构内容 -->
+        <tree-tools :tree-node="company" :is-root="true"></tree-tools>
         <!-- 树形结构 -->
         <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
           <!-- 传入插槽内容 -->
           <!-- 放置结构内容 -->
-          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
-            <el-col>
-              <!-- 左侧内容 -->
-              <span>{{ data.name }}</span>
-              <!-- /左侧内容 -->
-            </el-col>
-            <el-col :span="4">
-              <!-- 右侧内容 -->
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <!-- 放置下拉菜单 -->
-                  <el-dropdown>
-                    <!-- 内容 -->
-                    <span> 操作<i class="el-icon-arrow-down" /> </span>
-                    <!-- 具名插槽 -->
-                    <el-dropdown-menu slot="dropdown">
-                      <!-- 下拉选项 -->
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-              <!-- /右侧内容 -->
-            </el-col>
-          </el-row>
+          <tree-tools slot-scope="{ data }" :tree-node="data"></tree-tools>
           <!-- /传入插槽内容 -->
         </el-tree>
       </el-card>
@@ -65,12 +19,22 @@
 </template>
 
 <script>
+import TreeTools from './components/tree-tools.vue'
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
+      // 头部数据结构
+      company: {
+        name: '江苏传智播客教育科技股份有限公司',
+        manager: '负责人'
+      },
       defaultProps: {
         label: 'name' // 从name显示内容
       },
+      // 树形数据结构
       departs: [
         {
           name: '总裁办',
