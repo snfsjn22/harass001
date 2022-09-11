20f0fd2e915095faa7e19e0863e5932d2b9c040e
snfsjn22 <jh1710644231@163.com>
2022-09-11 19:44:31 +0800

    feat: 完成个人组件和岗位组件封装

--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -13,6 +13,14 @@ export default {
         title: '员工管理', // 左侧导航读取title属性
         icon: 'people'
       }
+    },
+    {
+      path: 'detail/:id', // query传参 动态路由传参
+      component: () => import('@/views/employees/detail'),
+      hidden: true, // 不在左侧菜单显示
+      meta: {
+        title: '员工详情' // 标记当前路由规则的中文名称 后续在做左侧菜单时 使用
+      }
     }
   ]
 }
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -8,8 +8,8 @@
         </template>
         <!-- 右侧显示按钮 excel导入 excel导出 新增员工 -->
         <template v-slot:after>
-          <el-button size="small" type="success">excel导入</el-button>
-          <el-button size="small" type="danger">excel导出</el-button>
+          <el-button size="small" type="success" @click="$router.push('/import')">excel导入</el-button>
+          <el-button size="small" type="danger" @click="exportData">excel导出</el-button>
           <el-button size="small" type="primary" @click="showDialog = true">新增员工</el-button>
         </template>
       </page-tools>
@@ -32,7 +32,7 @@
         </el-table-column>
         <el-table-column label="操作" sortable="" fixed="right" width="280">
           <template v-slot="{ row }">
-            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small" @click="$router.push(`/employees/detail/${row.id}`)">查看</el-button>
             <el-button type="text" size="small">转正</el-button>
             <el-button type="text" size="small">调岗</el-button>
             <el-button type="text" size="small">离职</el-button>
@@ -55,6 +55,7 @@
 import { getEmployeeList, delEmployee } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees' // 引入员工的枚举对象
 import AddEmployee from './components/add-employee.vue'
+import { formatDate } from '@/filters'
 export default {
   components: {
     AddEmployee
@@ -101,6 +102,47 @@ export default {
       } catch (error) {
         console.log(error)
       }
+    },
+    // 导出excel
+    exportData() {
+      const headers = {
+        姓名: 'username',
+        手机号: 'mobile',
+        入职日期: 'timeOfEntry',
+        聘用形式: 'formOfEmployment',
+        转正日期: 'correctionTime',
+        工号: 'workNumber',
+        部门: 'departmentName'
+      }
+      // 导出excel
+      import('@/vendor/Export2Excel').then(async (excel) => {
+        const { rows } = await getEmployeeList({ page: 1, size: this.page.total })
+        const data = this.formatJson(headers, rows)
+        const multiHeader = [['姓名', '主要信息', '', '', '', '', '部门']]
+        const merges = ['A1:A2', 'B1:F1', 'G1:G2']
+        excel.export_json_to_excel({
+          header: Object.keys(headers),
+          data,
+          filename: '员工资料表',
+          multiHeader,
+          merges
+        })
+      })
+    },
+    formatJson(headers, rows) {
+      return rows.map((item) => {
+        return Object.keys(headers).map((key) => {
+          if (headers[key] === 'timeOfEntry' || headers[key] === 'correctionTime') {
+            // 格式化日期
+            return formatDate(item[headers[key]])
+          } else if (headers[key] === 'formOfEmployment') {
+            const obj = EmployeeEnum.hireType.find((obj) => obj.id === item[headers[key]])
+            return obj ? obj.value : '未知'
+          }
+          return item[headers[key]]
+        })
+      })
+      // return rows.map((item) => Object.keys(headers).map((key) => item[headers[key]]))
     }
   }
 }
